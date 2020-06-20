5c7ca1d7d839411e7125ffabacbb2aea1f817a39
reinhash <reinhard.scheuerle@gmail.com>
2020-06-20 17:46:25 -0500

    added package for SSL redirect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var sslRedirect = require(‘heroku-ssl-redirect’);
 const express = require('express');
 const path = require('path');
 const e = require('express');
@@ -6,15 +7,11 @@ const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'reinhardcv/build')));
+app.use(sslRedirect());
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    if (req.headers["x-forwarded-proto"] === "https"){
-        res.sendFile(path.join(__dirname+'/reinhardcv/build/index.html'));
-    }
-    else {
-        res.redirect("https://" + req.headers.host + req.url); 
-    }
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/reinhardcv/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
